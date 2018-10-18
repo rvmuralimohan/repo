996e0b9cdc24a3193316a9c7d379d1864560982f
rvmuralimohan <mohanmurali.r@gmail.com>
2018-10-18 07:24:10 +0530

    new repo project for sonarqube

---
+++ src/main/java/com/java/citi/App.java
@@ -0,0 +1,13 @@
+package com.java.citi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
